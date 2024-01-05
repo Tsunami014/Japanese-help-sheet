a4643c2719751ef265b6a44a925076aa14824c7e
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2024-01-06 10:26:24 +1100

    refactor(main): 🔥 Removed testing.py and moved it's contents to __main__.py (the better file)

---
+++ __main__.py
@@ -0,0 +1,8 @@
+from utils.rendering import Window
+
+win = Window()
+
+r = True
+while r:
+    r = win.update()
+win.quit()
--- testing.py
+++
@@ -1,8 +0,0 @@
-from utils.rendering import Window
-
-win = Window()
-
-r = True
-while r:
-    r = win.update()
-win.quit()
